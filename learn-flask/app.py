7d93f0aa193611acced523e5dbbe8af43adaed7d
Jasper <colourfulgrey01@gmail.com>
2024-07-11 19:27:13 +0800

    Add minimal home page

---
+++ learn-flask/app.py
@@ -0,0 +1,23 @@
+from flask import url_for
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
